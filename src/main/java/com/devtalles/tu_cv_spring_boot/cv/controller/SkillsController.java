7e683369045ed0158d740e2cf78ced84e635ddc3
Dorian Gonzalez <doriandarren1@gmail.com>
2026-02-10 22:15:55 +0100

    add skills

--- src/main/java/com/devtalles/tu_cv_spring_boot/cv/controller/SkillsController.java
+++ src/main/java/com/devtalles/tu_cv_spring_boot/cv/controller/SkillsController.java
@@ -3,10 +3,7 @@ package com.devtalles.tu_cv_spring_boot.cv.controller;
 import com.devtalles.tu_cv_spring_boot.cv.model.Skill;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,13 +23,70 @@ import java.util.List;
 @RequestMapping("/skills")
 public class SkillsController {
 
-    private final List<Skill> skills = new ArrayList<>();
+//    private final List<Skill> skills = new ArrayList<>();
+    private final List<Skill> skills = new ArrayList<>(List.of(
+        new Skill("java", "Avanzado"),
+        new Skill("js", "Medio"),
+        new Skill("css", "Basico"),
+        new Skill("html", "Avanzado"),
+        new Skill("python", "Basico"),
+        new Skill("c++", "Avanzado"),
+        new Skill("c", "Medio")
+    ));
+
 
 
     @GetMapping
-    public String index(Model model) {
-        model.addAttribute("skills", skills);
+    public String index(
+            @RequestParam(defaultValue = "", required = false) String filter,
+            Model model
+    ) {
+
+        List<Skill> filteredSkills = skills.stream()
+                .filter(skill -> skill.getName().toLowerCase().contains(filter.toLowerCase()))
+                .toList();
+
+        model.addAttribute("skills", filteredSkills);
+        model.addAttribute("filter", filter);
+
         return "skills";
+    }
+
+
+    @GetMapping("/{index}")
+    public String show(
+            @PathVariable int index,
+            Model model
+    ){
+
+        if(index>=0 && index < skills.size()){
+            Skill skill = skills.get(index);
+            model.addAttribute("skill", skill);
+            return "show-skill";
+        }
+
+        return "redirect:/skills";
+    }
+
+
+    @GetMapping("/{name}/{level}")
+    public String showFilteredSkill(
+            @PathVariable String name,
+            @PathVariable String level,
+            Model model
+    ){
+
+        List<Skill> filteredSkills = skills.stream()
+                .filter(skill -> skill.getName().equalsIgnoreCase(name)
+                && skill.getLevel().equalsIgnoreCase(level)
+                )
+                .toList();
+
+
+        model.addAttribute("skills", filteredSkills);
+        model.addAttribute("filterMessage", "Filtro" + name + " - " + level);
+
+        return "/skills";
     }
 
 
@@ -48,7 +102,6 @@ public class SkillsController {
         skills.add(skill);
         return "redirect:/skills";
     }
-
 
 
 }
